5eac9b0f3d3c4486a124366ac878bc72f21565fe
Maks Burenko <Cherniymaks@yandex.ru>
2022-08-29 15:09:48 +0300

    123

--- src/streamAPI/Main.java
+++ src/streamAPI/Main.java
@@ -1,13 +1,10 @@
 package streamAPI;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import static java.util.stream.Collectors.toList;
+
 
 public class Main {
 
@@ -21,17 +18,16 @@ public class Main {
             BiConsumer<? super T, ? super T> minMaxConsumer) {
 
 
-        List<? extends T> elements = Arrays.stream();
+        List<? extends T> elements = stream.collect(Collectors.toList());
 
-
-
-            Optional <? extends T> min = stream.max(order);
-            Optional <? extends T> max = stream.min(order);
-
-            if (max.isPresent() && min.isPresent()) {
-                minMaxConsumer.accept(min.get(), max.get());
+            if (elements.get(0) != null) {
+                elements.sort(order);
+                minMaxConsumer.accept(Collections.max(elements, order), Collections.min(elements, order));
             } else {
                 minMaxConsumer.accept(null, null);
             }
     }
 }
+
+
+
